17d901280be649dbaf3c0b9aca6021d475342c7b
Harry Scells <harry@scells.me>
2026-02-05 15:47:46 +0100

    Make postcreatecommands more specific.

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,8 +1,3 @@
-uv run -m pip install /pybool_ir/pylucene/dist/*.whl
-export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
-export JCC_JDK=${JAVA_HOME}
-export JCC_ARGSEP=";"
-export JCC_LFLAGS="-L$JAVA_HOME/lib;-ljava;-L$JAVA_HOME/lib/server;-ljvm;-Wl,-rpath=$JAVA_HOME/lib:$JAVA_HOME/lib/server"
-export JCC="python -m jcc --wheel"
-uv add /pybool_ir/pylucene/jcc
+uv run -m pip install /pybool_ir/pylucene/dist/lucene-10.0.0-cp313-cp313-linux_x86_64.whl.whl
+JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64 JCC_JDK=${JAVA_HOME} JCC_ARGSEP=";" JCC_LFLAGS="-L$JAVA_HOME/lib;-ljava;-L$JAVA_HOME/lib/server;-ljvm;-Wl,-rpath=$JAVA_HOME/lib:$JAVA_HOME/lib/server" JCC="python -m jcc --wheel" uv add /pybool_ir/pylucene/jcc
 uv run -m pip install /pybool_ir
