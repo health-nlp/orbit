0fc76f9033124c1f5357b1ee855f47fd455669b3
Harry Scells <harry@scells.me>
2026-02-12 15:40:04 +0100

    Remove s from entrypoint.

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$MODE" = "pubmed" ]; then
 elif [ "$MODE" = "ctgov" ]; then
     echo baseline: $ORBIT_CTGOV_BASELINE_PATH
     echo index: $ORBIT_CTGOV_INDEX_PATH
-    uv run -m pybool_ir.cli ctgov download -b $ORBIT_CTGOV_BASELINE_PATH -s1
+    uv run -m pybool_ir.cli ctgov download -b $ORBIT_CTGOV_BASELINE_PATH
     uv run -m pybool_ir.cli ctgov index -b $ORBIT_CTGOV_BASELINE_PATH -i $ORBIT_CTGOV_INDEX_PATH -s1
 elif [ "$MODE" = "api" ]; then
     uv run -m fastapi dev main.py --host 0.0.0.0 --port 8333
